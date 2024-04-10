3950e7871d568c8b73fe56d45465bf20758aa758
Adhil Salim <bizadhil@gmail.com>
2024-04-10 19:43:03 +0530

    add doc - main.js

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -14,6 +14,26 @@ const heroSection = document.querySelector(".hero-main");
 const NavbarLinks = document.querySelectorAll(".nav-link");
 
 // ----------------- Navbar Link Hover Event Listeners ----------------- //
+
+/**
+ * This script adds mouse event listeners to each link in the navigation bar.
+ *
+ * For each link in the navigation bar (NavbarLinks), two event listeners are added:
+ *
+ * 1. "mouseenter": This event is triggered when the mouse pointer enters the link area.
+ *    When this event is triggered, the `mouseHovering` function is called with a string
+ *    argument in the format "enter-{navid}", where "{navid}" is the ID of the navigation
+ *    link as specified in the link's "data-navid" attribute.
+ *
+ * 2. "mouseleave": This event is triggered when the mouse pointer leaves the link area.
+ *    When this event is triggered, the `mouseHovering` function is called with a string
+ *    argument in the format "exit-{navid}", where "{navid}" is the ID of the navigation
+ *    link as specified in the link's "data-navid" attribute.
+ *
+ * The `mouseHovering` function is expected to handle these events appropriately,
+ * such as by changing the appearance of the navigation link to indicate that it is
+ * being hovered over.
+ */
 NavbarLinks.forEach((link) => {
   link.addEventListener("mouseenter", () =>
     mouseHovering(`enter-${link.dataset.navid}`)
@@ -24,7 +44,29 @@ NavbarLinks.forEach((link) => {
 });
 
 // ----------------- Navbar Auto Expand Animation ----------------- //
-
+/**
+ * This function controls the appearance of the navigation bar based on the intersection
+ * between the target element and its root container (viewport or a specified element).
+ *
+ * It takes an array of IntersectionObserverEntry objects as an argument. Each entry
+ * represents a change in the intersection status of a target element.
+ *
+ * For each entry:
+ *
+ * 1. If the target element is intersecting with the root container (i.e., it's visible
+ *    within the viewport or the specified element), the function adds the "nav-expand"
+ *    class to the navigation bar. This could, for example, expand the navigation bar or
+ *    make it fully visible.
+ *
+ * 2. If the target element is not intersecting with the root container (i.e., it's not
+ *    visible within the viewport or the specified element), the function removes the
+ *    "nav-expand" class from the navigation bar. This could, for example, collapse the
+ *    navigation bar or make it partially visible.
+ *
+ * This function is typically used as the callback function for an IntersectionObserver,
+ * which watches for changes in the intersection between the target element and its root
+ * container.
+ */
 const controlNavbar = (entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
@@ -36,7 +78,26 @@ const controlNavbar = (entries) => {
 };
 
 // ----------------- Navbar Link Hover Animation ----------------- //
-
+/**
+ * This function changes the background color of the navigation bar and the body of the
+ * document based on the navigation link being hovered over.
+ *
+ * It takes a string argument in the format "{action}-{navid}", where "{action}" is
+ * either "enter" or "exit" and "{navid}" is the ID of the navigation link.
+ *
+ * If "{action}" is "enter", this means that the mouse pointer has entered the area of
+ * the navigation link. The function changes the background color of the navigation bar
+ * and the body of the document to the color specified by the CSS variable
+ * "--nav-{navid}-background-color".
+ *
+ * If "{action}" is "exit", this means that the mouse pointer has left the area of the
+ * navigation link. The function changes the background color of the navigation bar to
+ * the color specified by the CSS variable "--nav-default-background-color" and the
+ * background color of the body of the document to "#f6f3f3".
+ *
+ * This function is typically used as the callback function for mouse event listeners
+ * added to the navigation links.
+ */
 const mouseHovering = (link) => {
   if (link.split("-")[0] === "enter") {
     navbar.style.background = `var(--nav-${
@@ -50,11 +111,23 @@ const mouseHovering = (link) => {
     document.body.style.background = "#f6f3f3";
   }
 };
-
 const observer = new IntersectionObserver(controlNavbar);
 observer.observe(heroSection);
 
 // ----------------- Audio Selection ----------------- //
+/**
+ * This event listener is triggered when the 'selectAudioButton' is clicked.
+ *
+ * Upon clicking, it creates a new 'input' element of type 'file' which accepts .mp3 and .wav files.
+ *
+ * It then assigns an 'onchange' event to the input element. This event is triggered when the user selects a file.
+ * The selected file is stored in the 'file' variable and cached using the 'cacheFile' function.
+ *
+ * After a file is selected, the text content of 'selectAudioButtonText' is changed to "Selected" and
+ * the 'light-green-bgcolor' class is added to 'selectAudioButton' to provide visual feedback to the user.
+ *
+ * Finally, the 'click' method is called on the input element to programmatically open the file selection dialog.
+ */
 selectAudioButton.addEventListener("click", () => {
   const input = document.createElement("input");
   input.type = "file";
@@ -71,6 +144,28 @@ selectAudioButton.addEventListener("click", () => {
 });
 
 // ----------------- Audio Upload ----------------- //
+/**
+ * This event listener is triggered when the 'uploadAudioButton' is clicked.
+ *
+ * Upon clicking, it checks if a file has been selected for upload. If no file has been selected,
+ * it shows a toast notification with an error message.
+ *
+ * If a file has been selected, it logs a message to the console, changes the text content of
+ * 'uploadAudioButton' to "Uploading...", and changes the button's background color to light green.
+ *
+ * It then creates a new FormData object and appends the selected file to it. This FormData object
+ * is used to send the file to the server.
+ *
+ * A fetch request is made to the server's '/upload' endpoint with the FormData object as the body.
+ * The request method is 'POST'.
+ *
+ * If the file is uploaded successfully, it logs a success message to the console and redirects the
+ * user to the '/studio/' page.
+ *
+ * If an error occurs during the upload, it changes the button's background color to light red,
+ * changes the button's text content to "Upload Failed", logs the error to the console, and shows a
+ * toast notification with an error message.
+ */
 uploadAudioButton.addEventListener("click", () => {
   if (file) {
     console.log("Uploading file", file);
@@ -114,6 +209,20 @@ uploadAudioButton.addEventListener("click", () => {
 });
 
 // ----------------- Cache File ----------------- //
+/**
+ * This function caches the name of the selected file in the local storage.
+ *
+ * It takes a 'file' object as an argument. If a file is provided, it stores the file's name
+ * in the local storage under the key 'upload_audio_cache' and logs a message to the console.
+ *
+ * If no file is provided (i.e., 'file' is null or undefined), it logs an error message to
+ * the console and shows a toast notification with the title "Cache Error", the type "error",
+ * the message "No file selected.", and a delay of 10 seconds.
+ *
+ * This function is typically used to cache the name of the file selected by the user for upload.
+ * The cached file name can be used to pre-populate the file selection input if the user leaves
+ * the page and comes back later.
+ */
 function cacheFile(file) {
   if (file) {
     localStorage.setItem("upload_audio_cache", file.name);
