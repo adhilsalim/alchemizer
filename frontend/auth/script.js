255cb68a5635c3105492659873ffb4f975c372cc
Adhil Salim <bizadhil@gmail.com>
2024-06-18 06:47:25 +0530

    add sign in and sign out

--- frontend/auth/script.js
+++ frontend/auth/script.js
@@ -4,51 +4,50 @@ import {
   AuthForgetPassword,
   AuthContinueWithGoogle,
   AuthIsLoggedIn,
-  AuthSignOut 
+  AuthSignOut,
 } from "./authentication.js";
 
 // ------------------------------ Toast ------------------------------ //
 const toastContainer = document.getElementById("main-toast-container");
 
 const showToast = (data) => {
-	try {
-	  if (data.type === "error") {
-		const errorAudio = new Audio("../audio/error.mp3");
-		errorAudio.play();
-	  } else if (data.type === "success") {
-		const successAudio = new Audio("../audio/success.mp3");
-		successAudio.play();
-	  } else if (data.type === "info") {
-		const infoAudio = new Audio("../audio/info.mp3");
-		infoAudio.play();
-	  }
-	} catch (error) {
-	  console.error("Error playing audio", error);
-	}
-  
-	const toastHTML = `<div class="toast" role="alert" aria-live="assertive" aria-atomic="true" style="color: ${
-	  data.type === "error" ? "#cf4444" : "black"
-	}"><div class="toast-header"><img src="" class="rounded me-2" alt=""><strong class="me-auto">${
-	  data.title
-	}</strong><small class="text-muted">${
-	  data.time || "just now"
-	}</small><button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button></div><div class="toast-body">${
-	  data.message
-	}</div></div>`;
-  
-	const toastElement = new DOMParser().parseFromString(toastHTML, "text/html")
-	  .body.firstChild;
-	const toast = new bootstrap.Toast(toastContainer.appendChild(toastElement), {
-	  delay: data.delay || 5000,
-	});
-  
-	const elementsToRemove = document.querySelectorAll("div.toast.fade.hide");
-  
-	elementsToRemove.forEach((element) => element.remove());
-  
-	toast.show();
-  };
+  try {
+    if (data.type === "error") {
+      const errorAudio = new Audio("../audio/error.mp3");
+      errorAudio.play();
+    } else if (data.type === "success") {
+      const successAudio = new Audio("../audio/success.mp3");
+      successAudio.play();
+    } else if (data.type === "info") {
+      const infoAudio = new Audio("../audio/info.mp3");
+      infoAudio.play();
+    }
+  } catch (error) {
+    console.error("Error playing audio", error);
+  }
 
+  const toastHTML = `<div class="toast" role="alert" aria-live="assertive" aria-atomic="true" style="color: ${
+    data.type === "error" ? "#cf4444" : "black"
+  }"><div class="toast-header"><img src="" class="rounded me-2" alt=""><strong class="me-auto">${
+    data.title
+  }</strong><small class="text-muted">${
+    data.time || "just now"
+  }</small><button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button></div><div class="toast-body">${
+    data.message
+  }</div></div>`;
+
+  const toastElement = new DOMParser().parseFromString(toastHTML, "text/html")
+    .body.firstChild;
+  const toast = new bootstrap.Toast(toastContainer.appendChild(toastElement), {
+    delay: data.delay || 5000,
+  });
+
+  const elementsToRemove = document.querySelectorAll("div.toast.fade.hide");
+
+  elementsToRemove.forEach((element) => element.remove());
+
+  toast.show();
+};
 
 // ------------------------------ Animation ------------------------------ //
 const signUpButton = document.getElementById("signUp");
@@ -73,47 +72,127 @@ signUpActionButton.addEventListener("click", () => {
 
   // check whether name and email are not empty
   if (name === "") {
-    alert("Please enter your name.");
+    showToast({
+      type: "error",
+      title: "Name Required",
+      message: "Please enter your name.",
+    });
     return;
   }
   if (email === "") {
-    alert("Please enter your email.");
+    showToast({
+      type: "error",
+      title: "Email Required",
+      message: "Please enter your email.",
+    });
     return;
   }
 
   // check whether password is at least 6 characters long and not empty
   if (password === "" || password.length < 6) {
-    alert("Please enter your password.");
+    showToast({
+      type: "error",
+      title: "Password Required",
+      message: "Please enter a password with at least 6 characters.",
+    });
     return;
   }
 
   AuthCreateAccount(name, email, password)
     .then((response) => {
       console.log(response);
-      window.location.href = "http://localhost:3000/studio/";
+      setTimeout(() => {
+        showToast({
+          type: "success",
+          title: "Account Created",
+          message: "You will be redirected to the studio in 2 seconds.",
+        });
+
+        setTimeout(() => {
+          window.location.href = "http://localhost:3000/studio/";
+        }, 4000);
+      }, 2000);
     })
     .catch((error) => {
       console.error(error);
-      alert("Error creating account: " + error.message);
+      showToast({
+        type: "error",
+        title: "Error Creating Account",
+        message: error.message,
+      });
     });
 });
 
 // ------------------------------ sign out ------------------------------ //
 const urlParams = new URLSearchParams(window.location.search);
 if (urlParams.has("signout")) {
-	AuthSignOut()
-  .then(response => {console.log(response);
-	showToast({
-			type: "info",
-			title: "Signed Out",
-			message: response.message,
-		});
-  })
-  .catch(error => {console.error(error)
-	showToast({
-			type: "error",
-			title: "Error Signing Out",
-			message: error.message,
-		});
-  });
+  AuthSignOut()
+    .then((response) => {
+      console.log(response);
+      showToast({
+        type: "info",
+        title: "Signed Out",
+        message: response.message,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      showToast({
+        type: "error",
+        title: "Error Signing Out",
+        message: error.message,
+      });
+    });
 }
+
+// ------------------------------ sign in ------------------------------ //
+const signInActionButton = document.getElementById("auth-sign-in-button");
+
+signInActionButton.addEventListener("click", () => {
+  const email = document.getElementById("auth-sign-in-email").value;
+  const password = document.getElementById("auth-sign-in-password").value;
+
+  // check whether email is not empty
+  if (email === "") {
+    showToast({
+      type: "error",
+      title: "Email Required",
+      message: "Please enter your email.",
+    });
+    return;
+  }
+
+  // check whether password is not empty
+  if (password === "") {
+    showToast({
+      type: "error",
+      title: "Password Required",
+      message: "Please enter your password.",
+    });
+    return;
+  }
+
+  AuthSignInWithEmailAndPassword(email, password)
+    .then((response) => {
+      console.log(response);
+      setTimeout(() => {
+        showToast({
+          type: "success",
+          title: "Signed In",
+          message: "You will be redirected to the studio in 2 seconds.",
+        });
+
+        setTimeout(() => {
+          window.location.href = "http://localhost:3000/studio/";
+        }, 4000);
+      }, 2000);
+    })
+    .catch((error) => {
+      console.error(error);
+      showToast({
+        type: "error",
+        title: "Error Signing In",
+        message: error.message,
+      });
+    });
+});
